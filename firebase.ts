4acd76c405f57270ac54f9f810be033d103d8a9d
Nathaniel Davis <nathaniel.davis@icsaddis.org>
2022-09-23 17:53:06 +0300

    First Commit

---
+++ firebase.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app"
+
+const app = initializeApp({
+  apiKey: "AIzaSyCbAntsZ8rlQVioXvwbknNdqGNWPZcOcOI",
+  authDomain: "ics-journalism.firebaseapp.com",
+  projectId: "ics-journalism",
+  storageBucket: "ics-journalism.appspot.com",
+  messagingSenderId: "222510070220",
+  appId: "1:222510070220:web:a07ffd41f82dad08438569",
+})
